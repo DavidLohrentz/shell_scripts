a988e6a677eb6b58505ecd2c1057543659e05414
David <david_lohrentz@yahoo.com>
2018-09-08 20:38:09 -0500

    Added while loop to make best of X match

--- rock.sh
+++ rock.sh
@@ -1,41 +1,81 @@
 #!/bin/bash
 
 echo "rock"
-sleep 0.5
+sleep 0.4
 echo "paper"
-sleep 0.5
+sleep 0.4
 echo "scissors"
-sleep 0.5
+sleep 0.4
 
-read -p "Make your selection: r/p/s: " HUMAN
+read -p "Match Length: " MATCH_LENGTH
+HUMAN_WINS=0
+COMPUTER_WINS=0
 
-if [ $HUMAN == "r" ]; then
-    HUMAN="rock"
+while [ $HUMAN_WINS -lt $MATCH_LENGTH -a $COMPUTER_WINS -lt $MATCH_LENGTH ]
+do
 
-elif [ $HUMAN == "p" ]; then
-    HUMAN="paper"
+    read -p "Make your selection: r/p/s: " HUMAN
 
-elif [ $HUMAN == "s" ]; then
-    HUMAN="scissors"
+    if [ $HUMAN == "r" ]; then
+        HUMAN="rock"
+
+    elif [ $HUMAN == "p" ]; then
+        HUMAN="paper"
+
+    elif [ $HUMAN == "s" ]; then
+        HUMAN="scissors"
+
+    else
+        read -p "Make a valid selection, dumb ass: 'rock', 'paper', or 'scissors'" HUMAN
+
+    fi
+
+    echo "Human's Selection is $HUMAN"
+
+    COMPUTER=$((1 + RANDOM % 3))
+
+
+    if [ $COMPUTER == 1 ]; then
+        COMPUTER_WORD="rock"
+
+    elif [ $COMPUTER == 2 ]; then
+        COMPUTER_WORD="paper"
+
+    else
+        COMPUTER_WORD="scissors"
+
+    fi
+
+
+    if [ $HUMAN == $COMPUTER_WORD ]; then
+        echo "Tie game. Shoot again."
+
+    elif [ $HUMAN == "rock" -a  $COMPUTER_WORD == "scissors" ]; then
+        let HUMAN_WINS++
+        echo "Rock smashes scissors. Human: $HUMAN_WINS, Computer: $COMPUTER_WINS"
+
+    elif [ $HUMAN == "scissors" -a  $COMPUTER_WORD == "paper" ]; then
+        let HUMAN_WINS++
+        echo "Scissors cuts paper. Human: $HUMAN_WINS, Computer: $COMPUTER_WINS"
+
+    elif [ $HUMAN == "paper" -a  $COMPUTER_WORD == "rock" ]; then
+        let HUMAN_WINS++
+        echo "Paper covers rock. Human wins! Human: $HUMAN_WINS, Computer: $COMPUTER_WINS"
+
+    else
+        let COMPUTER_WINS++
+        echo "Computer chose $COMPUTER_WORD. Human chose $HUMAN."
+        echo "Computer: $COMPUTER_WINS, Human: $HUMAN_WINS"
+
+
+    fi
+
+done
+
+if [ $COMPUTER_WINS -eq $MATCH_LENGTH ]; then
+    echo "Computer wins the match!"
 
 else
-    read -p "Make a valid selection, dumb ass: 'rock', 'paper', or 'scissors'" HUMAN
+    echo "Human won the match, $HUMAN_WINS to $COMPUTER_WINS."
 
 fi
-
-echo "Human's Selection is $HUMAN"
-
-COMPUTER=$((1 + RANDOM % 3))
-echo "Computer's numeric selection is $COMPUTER"
-
-if [ $COMPUTER == 1 ]; then
-    COMPUTER_WORD="rock"
-
-elif [ $COMPUTER == 2 ]; then
-    COMPUTER_WORD="paper"
-
-else
-    COMPUTER_WORD="scissors"
-
-fi
-echo "Computer's converted selection is $COMPUTER_WORD"
